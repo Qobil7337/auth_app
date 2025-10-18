1bf517aea3fe79b7109810b64a2839e625f8a4d8
Qobil7337 <qobil7337@mail.ru>
2025-10-18 18:48:32 +0500

    add some part of sign up page

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -10,6 +12,158 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Text('Hello')));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   backgroundColor: Colors.white,
+      //   elevation: 0,
+      //   leading: IconButton(
+      //     onPressed: () {
+      //       Navigator.pop(context);
+      //     },
+      //     icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+      //   ),
+      // ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(24.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Back Arrow
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.black,
+                        size: 22.w,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 32.h),
+
+                  // Title
+                  Text(
+                    'Sign up',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+
+                  // Text
+                  Text(
+                    'Create an account to continue!',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 11.sp, color: Colors.grey),
+                  ),
+                  SizedBox(height: 32.h),
+
+                  // Full Name Input
+                  Text('Full Name', style: TextStyle(color: Colors.grey)),
+                  SizedBox(height: 4.h),
+                  TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 12.h,
+                        horizontal: 12.w,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+
+                  // Email Input
+                  Text('Email', style: TextStyle(color: Colors.grey)),
+                  SizedBox(height: 4.h),
+                  TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 12.h,
+                        horizontal: 12.w,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+
+                  // Date Input
+                  Text('Date of Birth ', style: TextStyle(color: Colors.grey)),
+                  SizedBox(height: 4.h),
+                  TextField(
+                    readOnly: true,
+                    controller: TextEditingController(text: '18/03/2024'),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      suffixIcon: Padding(
+                        padding: EdgeInsets.all(12.w),
+                        child: SvgPicture.asset(
+                          'lib/assets/icons/calendar.svg',
+                          width: 20.w,
+                          height: 20.h,
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 12.h,
+                        horizontal: 12.w,
+                      ),
+                    ),
+                    onTap: () async {
+                      FocusScope.of(
+                        context,
+                      ).requestFocus(FocusNode()); // close keyboard
+                      final DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2100),
+                      );
+                      if (pickedDate != null) {
+                        setState(() {
+                          // _dateController.text =
+                          // "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                        });
+                      }
+                    },
+                  ),
+
+                  // Phone Number Input
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
